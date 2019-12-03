47889fcf300519690b40fe5901f946353f201e99
chengxiongchao <1223408988@qq.com>
2019-12-03 15:03:05 +0800

    MOD: getUploadLinkFromFormToken -> getUploadLinkViaFormToken

--- src/dtable-web-api.js
+++ src/dtable-web-api.js
@@ -771,7 +771,7 @@ class DTableWebAPI {
     return this.req.put(url, formData);
   }
 
-  getUploadLinkFromFormToken(token) {
+  getUploadLinkViaFormToken(token) {
     let url = this.server + '/api/v2.1/dtable-forms/' + token + '/upload-link/';
     return this.req.get(url);
   }
