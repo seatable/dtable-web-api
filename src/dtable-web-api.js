f6beb24a0d0af52ad559693a07d8f1a822e3f4c4
‘JoinTyang’ <yangtong1009@163.com>
2022-04-02 11:41:10 +0800

    update

--- src/dtable-web-api.js
+++ src/dtable-web-api.js
@@ -3080,8 +3080,8 @@ class DTableWebAPI {
     return this.req.delete(url);
   }
 
-  sysAdminListCommonDatasetSyncs(page, perPage) {
-    let url = this.server + '/api/v2.1/admin/common-dataset/syncs/';
+  sysAdminListCommonDatasets(page, perPage) {
+    let url = this.server + '/api/v2.1/admin/common-datasets/';
     let params = {
       page: page,
       per_page: perPage
