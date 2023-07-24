17935f08d03095433256cb1f104f302cb71fcfb6
r350178982 <32759763+r350178982@users.noreply.github.com>
2023-07-24 12:10:01 +0800

    Update dtable-web-api.js

--- src/dtable-web-api.js
+++ src/dtable-web-api.js
@@ -1118,6 +1118,14 @@ class DTableWebAPI {
     return this.req.post(url);
   }
 
+  suspendExternalAppInstance(token, isPause) {
+    let url = this.server + '/api/v2.1/external-apps/' + token + '/suspend/';
+    let data = {
+      is_pause: isPause
+    };
+    return this.req.put(url, data);
+  }
+
   listUserApps() {
     let url = this.server + '/api/v2.1/universal-apps/';
     return this.req.get(url);
