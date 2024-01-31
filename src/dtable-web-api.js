0f988512a351be71251abec126b617ba4549f8a4
r350178982 <32759763+r350178982@users.noreply.github.com>
2024-01-31 13:29:51 +0800

    Update dtable-web-api.js

--- src/dtable-web-api.js
+++ src/dtable-web-api.js
@@ -1592,8 +1592,8 @@ class DTableWebAPI {
     return this._sendPostRequest(url, data, {headers: {'Content-Type': 'application/json'}});
   }
 
-  getThirdPartyAccountsDetail(dtableUuid, account_name) {
-    let url = this.server + '/api/v2.1/third-party-accounts/' + dtableUuid + '/detail/?account_name=' + encodeURIComponent(account_name);
+  getThirdPartyAccountsDetail(dtableUuid, account_id) {
+    let url = this.server + '/api/v2.1/third-party-accounts/' + dtableUuid + '/' + account_id + '/';
     return this.req.get(url);
   }
 
@@ -1602,24 +1602,22 @@ class DTableWebAPI {
     return this.req.get(url);
   }
 
-  addThirdPartyAccount(dtableUuid, account_name, accout_type, detail, ownerID) {
+  addThirdPartyAccount(dtableUuid, account_name, accout_type, detail) {
     let url = this.server + '/api/v2.1/third-party-accounts/' + dtableUuid + '/';
     let data = {
       'account_name': account_name,
       'account_type': accout_type,
       'detail': detail,
-      'owner': ownerID
     };
     return this._sendPostRequest(url, data, {headers: {'Content-Type': 'application/json'}});
   }
 
-  updateThirdPartyAccount(dtableUuid, account_id, account_name, account_type, detail, ownerID) {
+  updateThirdPartyAccount(dtableUuid, account_id, account_name, account_type, detail) {
     let url = this.server + '/api/v2.1/third-party-accounts/' + dtableUuid + '/' + account_id + '/';
     let data = {
       'account_name': account_name,
       'account_type': account_type,
       'detail': detail,
-      'owner': ownerID
     };
     return this.req.put(url, data, {headers: {'Content-Type': 'application/json'}});
   }
@@ -2065,9 +2063,9 @@ class DTableWebAPI {
     return this.req.post(url, params);
   }
 
-  listGroups(includingAllDeps=false, canAdmin=false) {
+  listGroups(includingAllDeps=false) {
     const url = this.server + '/api/v2.1/groups/';
-    let params = {including_all_deps: includingAllDeps, can_admin: canAdmin};
+    let params = {including_all_deps: includingAllDeps};
     return this.req.get(url, {params: params});
   }
 
