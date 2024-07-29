eea7969ab572988808f9021bbebb8ad6724fe547
Junxiang Huang <wacmkxiaoyi@gmail.com>
2024-07-29 16:20:41 +0800

    feat: support audit-group log

--- src/dtable-web-api.js
+++ src/dtable-web-api.js
@@ -4601,6 +4601,15 @@ class DTableWebAPI {
     return this.req.delete(url);
   }
 
+  sysAdminListAuditGroupLogs(page, perPage) {
+    let url = this.server + '/api/v2.1/admin/audit-logs/group/';
+    let params = {
+      page: page,
+      per_page: perPage
+    };
+    return this.req.get(url, { params: params });
+  }
+
   sysAdminListAdminLogs(page, perPage) {
     let url = this.server + '/api/v2.1/admin/admin-logs/';
     let params = {
